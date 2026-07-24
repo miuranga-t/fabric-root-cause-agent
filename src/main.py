acbeabe8dd638af47c39d9f4979a8f3b27233e3e
miuranga-t <theekshanamiuranga4@gmail.com>
2026-07-24 11:13:30 +0530

    Added investigation engine testing
    Added functionality to detect missing records and duplicates.

--- src/main.py
+++ src/main.py
@@ -8,3 +8,18 @@ result = engine.compare_totals(
 )
 
 print(result)
+
+# Missing Records
+missing = engine.detect_missing_records(
+    dashboard_records=[1001,1002,1003],
+    source_records=[1001,1002,1003,1004,1005]
+)
+
+print("Missing:", missing)
+
+# Duplicates
+duplicates = engine.detect_duplicates(
+    [1001,1002,1003,1002,1004,1001]
+)
+
+print("Duplicates:", duplicates)
