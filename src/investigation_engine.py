4c03218c72a2a2c43fa9039cefc7a8dcc1a5dee6
miuranga-t <theekshanamiuranga4@gmail.com>
2026-07-24 10:44:36 +0530

    Added total comparison logic

--- src/investigation_engine.py
+++ src/investigation_engine.py
@@ -1,2 +1,11 @@
 class InvestigationEngine:
-    pass
+
+    def compare_totals(self, dashboard_total, source_total):
+
+        difference = source_total - dashboard_total
+
+        return {
+            "dashboard_total": dashboard_total,
+            "source_total": source_total,
+            "difference": difference
+        }
